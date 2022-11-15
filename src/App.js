2a27f8e926554eb2cd16ddd59d719ce637270db5
aneeqasghar <your_email@example.com>
2022-11-16 00:33:30 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { Fragment } from "react";
+import Banner from "./components/Banner";
+import Solutions from "./components/Solutions";
+import Services from "./components/Services";
+import MakeIT from "./components/MakeIT";
+import Consultation from "./components/Consultation";
+import Relation from "./components/Relation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Banner />
+      <Solutions />
+      <Services />
+      <MakeIT />
+      <Consultation />
+      <Relation />
+      <Footer />
+    </Fragment>
   );
 }
 
